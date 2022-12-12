01ab7206579b547d63c29975a38904c0184021f7
Javier Fadel <javierfadel1208@gmail.com>
2022-12-12 20:14:06 +0700

    This commit will update Minggu-12 assignment.

--- minggu-12/app.js
+++ minggu-12/app.js
@@ -1,19 +1,64 @@
-let text = document.getElementById('text')
-const car = {
-    name: 'Toyota', 
-    wheel: 'Four wheels',
-    doors: 'Four doors', 
-    showCar: () => {
-        console.log(`Car name: ${this.name}, wheel: ${this.wheel}, doors: ${this.doors}`)
+let container = document.querySelector('.container'),
+    form = document.getElementById('inputForm'),
+    inputName = document.getElementById('inputName'),
+    inputSubmit = document.getElementById('inputSubmit'),
+    table = document.getElementById('tableMahasiswa'),
+    size = document.getElementById('sizeNumber'),
+    isMore = false
+
+let names = ['ben', 'tom', 'jenner']
+const updateData = function() {
+    for (let [i, name] of names.entries()) {
+        table.innerHTML +=
+        `<tr class="table-body__row">
+            <td class="table-body__row-item">
+                ${i + 1}
+            </td>
+            <td class="table-body__row-item">
+                ${name}
+            </td>
+            <td class="table-body__row-item">
+                <button id="#uploadButton" class="btn btn--upload">Upload</button>
+                <button id="#deleteButton" class="btn btn--delete">Delete</button>
+            </td>
+        </tr>`
+        size.innerHTML = names.length
     }
 }
 
-// #FFFFFF or RGB(255, 255, 255)
-let changeColor = () => {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16)
-    document.body.style.backgroundColor = '#' + randomColor
-}
+// TODO: this function will stack a new array to another.
+updateData();   
 
-setInterval(() => {
-    changeColor()
-}, 1000)
+form.addEventListener('submit', () => {
+    names.push(inputName.value)
+    if (names.length <= 10) {
+        table.innerHTML += 
+            `<tr class="table-body__row">
+                <td class="table-body__row-item">
+                    ${names.length}
+                </td>
+                <td class="table-body__row-item">
+                    ${inputName.value}
+                </td>
+                <td class="table-body__row-item">
+                    <button id="#deleteButton" class="btn btn--upload">Upload</button>
+                    <button id="#deleteButton" class="btn btn--delete">Delete</button>
+                </td>
+            </tr>`
+
+            size.innerHTML = names.length
+        inputName.value = ''
+    } else if (!isMore) {
+        container.innerHTML += 
+            `<a class="table-body__link" href="#">Load more..</a>`
+        isMore = true
+    }
+})
+
+// If the name is more than 10, switch to a link that can load more.
+
+// const deleteData = () => {
+
+// }
+
+let btn = document.getElementsByClassName('btn--delete')
