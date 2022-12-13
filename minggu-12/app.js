7a20893257ad9dd4f191af4dc6d5c3dc2f22bdb1
Javier Fadel <javierfadel1208@gmail.com>
2022-12-13 10:11:46 +0700

    This commit will add the final version of dynamic list.

--- minggu-12/app.js
+++ minggu-12/app.js
@@ -18,8 +18,8 @@ const updateData = function() {
                 ${name}
             </td>
             <td class="table-body__row-item">
-                <button id="#uploadButton" class="btn btn--upload">Upload</button>
-                <button id="#deleteButton" class="btn btn--delete">Delete</button>
+                <button id="updateButton${i + 1}" class="btn btn--update">Update</button>
+                <button id="deleteButton${i + 1}" class="btn btn--delete">Delete</button>
             </td>
         </tr>`
         size.innerHTML = names.length
@@ -41,8 +41,8 @@ form.addEventListener('submit', () => {
                     ${inputName.value}
                 </td>
                 <td class="table-body__row-item">
-                    <button id="#deleteButton" class="btn btn--upload">Upload</button>
-                    <button id="#deleteButton" class="btn btn--delete">Delete</button>
+                    <button id="deleteButton${names.length}" class="btn btn--update">Update</button>
+                    <button id="deleteButton${names.length}" class="btn btn--delete">Delete</button>
                 </td>
             </tr>`
 
@@ -55,10 +55,70 @@ form.addEventListener('submit', () => {
     }
 })
 
-// If the name is more than 10, switch to a link that can load more.
+// TODO: If the name is more than 10, switch to a link that can load more.
 
-// const deleteData = () => {
+// Every id must be unique, don't do this 🙅‍♂️.
+    // const deleteButton = document.getElementById('deleteButton')
+    // deleteButton.addEventListener('click', () => {
+    //     alert('clicked')
+    // })
+// Done ✅.
 
-// }
+const deleteButton = document.querySelectorAll('.btn--delete')
+deleteButton.forEach((btn) => {
+    btn.addEventListener('click', (el) => {
+        // Find a way to return the current <tr>, then delete it, along with the data on the arrays.
+        // Currently every button has a unique id's, could we use it?
+        
+        // We could access the target or `this` object via this target object.
+        // This bellow will map the target object.
+        console.dir(el.target)
+        // Get the button's id via this code.
+        console.log(el.target.id)
+        // Access the parent-parent element (or grandparent).
+        console.dir(el.target.parentNode.parentNode)
+        // This will return the name of the second td's.
+        console.dir(el.target.parentNode.parentNode.children[1].innerText)
+        
+        // Removing the selected element.
+        // This function is the shit.
+        // el.target.parentNode.parentNode.remove()
 
-let btn = document.getElementsByClassName('btn--delete')
+        // Step 0: get the current element. 
+        const buttonId = el.target.id;
+
+        // Step 1: get the value of the name you want to delete, use it as an index to delete the data on the arrays.
+        // Step 1.1: This code below will return index of the value `name`.
+        const name = el.target.parentNode.parentNode.children[1].innerText
+        names.indexOf(name.toLowerCase()) !== -1 && names.splice(names.indexOf(name.toLowerCase()), 1)
+
+        // Step 2: select the current id, use it as an index for the selector.
+        // Step 2.1: ask for confirmation.
+        const confirmation = confirm(`Are you sure want to delete ${name} from the list?`)
+        if (confirmation) {
+            // Step 3: delete 👋.
+            el.target.parentNode.parentNode.remove()
+        }
+    })
+})
+
+const updateButton = document.querySelectorAll('.btn--update')
+updateButton.forEach((btn) => {
+    btn.addEventListener('click', (el) => {
+        // Find a way to return the current <tr>, then update it, along with the data on the arrays.
+        // Step 0: get the current element, then the parent-parent.
+
+        // Step 1: get the name value of the current <tr>, use it as an index to update the array.
+        const name = el.target.parentNode.parentNode.children[1].innerText
+        const index = names.indexOf(name.toLowerCase())
+        
+        // Step 2: update data.
+        const newName = prompt('Enter new name: ')
+        if (newName) {
+            names[index] = newName
+        }
+
+        // Step 3: Update view.
+        el.target.parentNode.parentNode.children[1].innerText = newName
+    })
+})
